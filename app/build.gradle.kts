012ad96d9658ebd57d5c859aa9b48f2cf61d97a8
Harsh Agarwal <alwaysharshgarg@gmail.com>
2026-01-01 22:58:22 +0530

    socket build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 
 android {
     namespace = "com.facemash.app"
+
     compileSdk {
         version = release(36)
     }
@@ -48,6 +49,14 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.accompanist:accompanist-swiperefresh:0.34.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    // ✅ Socket.IO client
+    implementation("io.socket:socket.io-client:2.1.0") {
+        exclude(group = "org.json", module = "json")
+    }
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,9 +65,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
